4f7da92cb8281ea200b8e54f550ff5efb96f1fae
Toann-nguyen <nguyenminhtoan2712py@gmail.com>
2025-05-12 13:23:46 +0700

    code sign in google

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -10,9 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -26,8 +28,10 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.common.SignInButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements GoogleSignInHelper.GoogleSignInCallback  {
 
     // khai báo biến cho các thành phần giao diện
     private EditText usernameEditText;
@@ -37,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     private Button registerButton;
     private FirebaseAuth auth;
 
+    private GoogleSignInHelper googleSignInHelper;
+    private SignInButton googleSignInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Khởi tạo FirebaseAuth
         auth = FirebaseAuth.getInstance();
+        // Khởi tạo GoogleSignInHelper
+        googleSignInHelper = new GoogleSignInHelper(this, this);
 
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
@@ -51,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         messageTextView = findViewById(R.id.messageTextView);
         // dang ky
         registerButton = findViewById(R.id.registerButton);
+        googleSignInButton = findViewById(R.id.googleSignInButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,28 +80,36 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-                                    // Đăng nhập thành công
                                     messageTextView.setText("Đăng nhập thành công");
                                     Intent intent = new Intent(MainActivity.this, HomeMainActivity.class);
                                     startActivity(intent);
+                                    finish();
                                 } else {
                                     Exception e = task.getException();
                                     if (e instanceof FirebaseAuthInvalidUserException) {
-                                        // Tài khoản chưa tồn tại
                                         messageTextView.setText("Không có tài khoản");
                                     } else if (e instanceof FirebaseAuthInvalidCredentialsException) {
-                                        // Sai email hoặc mật khẩu
                                         messageTextView.setText("Email hoặc mật khẩu không đúng");
                                     } else if (e instanceof FirebaseNetworkException) {
-                                        // Lỗi mạng từ Firebase
                                         messageTextView.setText("Đăng nhập thất bại: Lỗi mạng, vui lòng kiểm tra kết nối");
                                     } else {
-                                        // Các lỗi khác
                                         messageTextView.setText("Đăng nhập thất bại: " + e.getLocalizedMessage());
                                     }
                                 }
                             }
                         });
+            }
+        });
+        // Thiết lập sự kiện cho nút đăng nhập bằng Google
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isNetworkAvailable()) {
+                    messageTextView.setText("Không có kết nối internet");
+                    return;
+                }
+                // Gọi phương thức đăng nhập từ GoogleSignInHelper
+                googleSignInHelper.signIn();
             }
         });
         // chuyen sang trang RegisterActivity
@@ -103,6 +121,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Chuyển kết quả cho GoogleSignInHelper xử lý
+        googleSignInHelper.handleActivityResult(requestCode, resultCode, data);
     }
     private boolean isNetworkAvailable() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -122,15 +146,29 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
     @Override
     protected void onStart() {
         super.onStart();
-        // Kiểm tra nếu người dùng đã đăng nhập, hiển thị thông báo
-        // Nếu người dùng đã đăng nhập, chuyển sang một activity khác (ví dụ: trang chính)
         if (auth.getCurrentUser() != null) {
             Intent intent = new Intent(MainActivity.this, HomeMainActivity.class);
             startActivity(intent);
-          //  finish(); // Kết thúc MainActivity để không quay lại
+            finish();
         }
+    }
+
+    @Override
+    public void onSignInSuccess(FirebaseUser user) {
+        String userName = user.getDisplayName();
+        Toast.makeText(this, "Xin chào " + userName, Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(MainActivity.this, HomeMainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onSignInFailure(Exception e) {
+        messageTextView.setText("Đăng nhập Google thất bại: " + e.getMessage());
     }
 }
